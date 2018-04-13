5d9f17442ad748f2cf57d53f2f44f99564ba14f5
Yan Siegers <email@yansiegers.nl>
2018-04-13 14:11:25 +0200

    v0.0.24 - added js class files for tracking - added a js class files for assessing

---
+++ BroemmVS/BroemmVS/www/scripts/drivingAssessor.js
@@ -0,0 +1,17 @@
+﻿import "accellerationTracker";
+import "positionTracker";
+
+function drivingAssessor() {
+    // Error handling
+    this.onError(error) {
+        // Error notification
+        console.log(
+            'code: ' + error.code + '\n' +
+            'message: ' + error.message + '\n'
+        );
+    }
+
+    this.assessPullup(positions, accelerations) {
+        /* code here */
+    }
+}
---
+++ BroemmVS/BroemmVS/www/scripts/positionTracker.js
@@ -0,0 +1,14 @@
+﻿function positionTracker() {
+    // Error handling
+    this.onError(error) {
+        // Error notification
+        console.log(
+            'code: ' + error.code + '\n' +
+            'message: ' + error.message + '\n'
+        );
+    }
+
+    this.trackPosition(unit) {
+        /* code here */
+    }
+}
