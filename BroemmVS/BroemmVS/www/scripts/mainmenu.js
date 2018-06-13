a478890ca2bd76e0b2c8b4829574e17e3135af6a
Yan Siegers <email@yansiegers.nl>
2018-06-13 11:13:56 +0200

    v0.3.5 - made the beer fully swipable (right and back, bottom) - added some animations (not fully implemented)

--- BroemmVS/BroemmVS/www/scripts/mainmenu.js
+++ BroemmVS/BroemmVS/www/scripts/mainmenu.js
@@ -9,18 +9,68 @@
         $(document).ready(function () {
             console.log("Document is ready!");
 
-            $("#bierglas .beer").hammer().on("swipe", function (event) {
+            var totalpages = 3;
+            var currentpage = 2;  // middle
+
+            var $startstop = $("#startstop");
+            var $tap = $(".tap");
+
+            /*$("#bierglas .beer").hammer().on("swipedown", function (event) {
+                console.log("swipe down gesture detected");
+            }*/
+            var $beer = $("#bierglas .beer");
+            var beerhammer = $beer.hammer();
+            $beer.data('hammer').get('swipe').set({ direction: Hammer.DIRECTION_ALL });
+
+            beerhammer.on("swipe", function (event) {
                 console.log(event.gesture.direction + " gesture detected");
 
-                if (event.gesture.direction === 2) {
-                    //swipe to left
-                    $(".tap").css("display", "block");
-                    $("#startstop").css("display", "none");
-                }
+                if (event.gesture.direction === 2) { // right -> left
+                    console.log("gesture right -> left detected");
 
-                else if (event.gesture.direction === 4) {
-                    //swipe to right
-                    window.location.href = "feedbackmenu.html"; // Redirect
+                    if (currentpage < totalpages) {
+                        currentpage += 1;
+                        //console.log("page: " + currentpage);
+
+                        switch (currentpage) {
+                            case 2:
+                                // there's no case
+                                break;
+                            case 3:
+                                //$startstop.addClass("slideOutUp");
+                                //$startstop.removeClass("slideinDown");
+                                $startstop.css("display", "none");
+
+                                $tap.css("display", "block");
+                                //$tap.removeClass("slideOutLeft");
+                                //$tap.addClass("slideInRight");
+                                break;
+                        };
+                    };
+                } else if (event.gesture.direction === 4) { // right <- left 
+                    console.log("gesture right <- left detected");
+
+                    if (currentpage > 1) {
+                        currentpage -= 1;
+                        //console.log("page: " + currentpage);
+
+                        switch (currentpage) {
+                            case 2:
+                                //$startstop.removeClass("slideOutUp");
+                                //$startstop.addClass("slideinDown");
+                                $startstop.css("display", "block");
+
+                                //$tap.removeClass("slideInRight");
+                                //$tap.addClass("slideOutLeft");
+                                $tap.css("display", "none");
+                                break;
+                            case 1:
+                                window.location.href = "feedbackmenu.html"; // Redirect
+                                break;
+                        };
+                    };
+                } else if (event.gesture.direction === 8) {
+                    console.log("gesture top -> bottom detected");
                 };
             });
             
@@ -30,17 +80,19 @@
 
             $startstop.click(function () {
                 if (clicked == false) {
-                    console.log("clicked! turn red!");
+                    //console.log("clicked! turn red!");
                     $startstop.css("background", "#C81A1A");
                     $startstop.css("border", "5px solid #AE1717");
+                    $("#bierglas .beer").animate({ height: "50vh" });
                     $("#startstop p").html("Stop rit");
 
                     clicked = true;
                 } else {
-                    console.log("clicked! turn green again!");
+                    //console.log("clicked! turn green again!");
                     window.location.href = "feedbackmenu.html"; // Redirect
                     $startstop.css("background", "#6ABA36");
                     $startstop.css("border", "5px solid #428616");
+                    $("#bierglas .beer").animate({height: "40vh"});
                     $("#startstop p").html("Start");
 
                     clicked = false;
